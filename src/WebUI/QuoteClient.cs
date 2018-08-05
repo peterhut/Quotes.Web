6341945cbfdcf695685a63255983df0610d9972b
Peter Hut <peter.h.hut@cgi.com>
2018-08-05 20:21:12 +0200

    Completed docker build

--- src/WebUI/QuoteClient.cs
+++ src/WebUI/QuoteClient.cs
@@ -14,7 +14,7 @@ namespace WebUI
         public QuoteClient(HttpClient client, ILogger<QuoteClient> logger, IConfiguration config)
         {
             _client = client;
-            _client.BaseAddress = new Uri(config["QuotesService"]);
+            _client.BaseAddress = new Uri(config["QuoteService"]);
             _logger = logger;
         }
 
