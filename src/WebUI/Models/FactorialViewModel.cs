daa13894e0bf99d3d1c66a38dba6c4efcb19d6b7
Peter Hut <peter.h.hut@cgi.com>
2018-08-12 15:50:29 +0200

    Changed to MVC and removed test project.

---
+++ src/WebUI/Models/FactorialViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+{
+    public class FactorialViewModel
+    {
+        [FromForm]
+        public long N { get; set; }
+
+        public BigInteger FactorialOfN { get; set; }
+    }
+}
--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -76,13 +76,19 @@ namespace WebUI
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("Home/Error");
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
 
 
